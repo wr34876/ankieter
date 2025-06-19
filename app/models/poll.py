e3a5a48eb764b2bb7604178588a3a6a103eb3618
Kawetnick <kacper_2002@onet.pl>
2025-06-19 14:08:04 +0200

    Zmiany w poll.py

--- app/models/poll.py
+++ app/models/poll.py
@@ -5,3 +5,4 @@ class Poll(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id')) 
